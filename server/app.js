b9c566d9421a94a3316536d926f5235cc10be0c5
Kris Szafranski <kdszafranski@gmail.com>
2016-11-08 14:57:29 -0600

    working get request partial post

--- server/app.js
+++ server/app.js
@@ -5,12 +5,27 @@ var path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 
+// data
+var songs = [
+  {
+    artist: "Bruce Springstein",
+    title: "Born in the U.S.A."
+  }
+];
+
+// Routes
+app.get('/songs', function(req, res) {
+  console.log('handling get request for songs');
+  // response options
+  // res.sendStatus(200);
+  res.send(songs);
+});
 
 // static file routing
 app.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
   console.log(file);
-  
+
   res.sendFile(path.join(__dirname, './public/', file));
   // /public/views/index.html
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,51 @@
 $(document).ready(function() {
   console.log("it's alive!");
+
+  $("#postSongForm").on("submit", function(event) {
+    event.preventDefault();
+    var newSong = {};
+
+    $.each($('#postSongForm').serializeArray(), function(i, field) {
+      newSong[field.name] = field.value;
+    });
+
+    console.log(newSong);
+
+    // send song object to the Server
+    $.ajax({
+      type: 'POST',
+      url: '/songs',
+      success: function(response) {
+        console.log(response);
+      }
+    })
+
+  })
+
+  getSongs();
+
+  function getSongs() {
+    $.ajax({
+      type: 'GET',
+      url: '/songs',
+      success: function(songData) {
+        songsToDom(songData);
+      }
+    });
+  }
+
+  function songsToDom(songs) {
+    $("#songContainer").empty();
+
+    for (var i = 0; i < songs.length; i++) {
+      $("#songContainer").append('<div class="song"></div>');
+      var $el = $("#songContainer").children().last();
+      $el.append('<h3>' + songs[i].title + '</h3>');
+      $el.append('<p>By: ' + songs[i].artist + '</p>');
+    }
+
+  }
+
+
+
 });
